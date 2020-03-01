6c45dbab2c63ecedd4e738cece48067017c98445
Anand <anand.a.anand@capgemini.com>
2020-03-01 11:00:31 +1100

    sample commit

--- redux--01-start/src/store/actions.js
+++
@@ -1,6 +0,0 @@
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
-export const ADD = 'ADD';
-export const SUBSTRACT = 'SUBSTRACT';
-export const STORE_RESULT = 'STORERESULT';
-export const DELETE_RESULT = 'DELETERESULT';
